1bc012fae504343e7a30c96c01a32d8bc0346868
Kris Lee <kdlee0401@gmail.com>
2026-06-24 15:56:28 -0500

    Got the skeleton of pseudocode, just need to actually code it later.

---
+++ quiz.js
@@ -0,0 +1,46 @@
+// Kris Lee 6/24/2026
+"use strict";
+
+/* PLANNIN - PSEUDOCODE
+
+1. Initialize score variable name will be 'currentScore' set to 0.
+
+2. Question 1: [Your question here]
+    - Variable name personalized to question (no answer1)
+    - Correct answer:
+    - How I'll validate and check the answer:
+
+3. Use that structure for all four questions.
+
+6. Math: How will I calculate the percentage?
+
+7. Final feedback logic (if / else if / else):
+
+Logical operators I will use:
+- && for ...
+- || or ! for ...
+*/
+
+console.log("=== My Quiz Game ===");
+
+// Initialize score
+
+// Question 1
+
+// Question 2
+
+// Question 3
+
+// Question 4
+
+// Calculate percentage using math
+
+// Final feedback using if / else if / else
+
+//If statement that uses and alert statement about their score
+
+//(correct answers = 3 or 4 or less than 2 or = 0)
+
+// Additional feedback with logical operators
+
+// Final summary alert
